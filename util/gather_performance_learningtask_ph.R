7bee4cbeb91eaba40a61c047d7b10589d4eb8f00
Patrick Haller <patrick.hlr@gmail.com>
2019-02-27 08:34:03 +0100

    update R script for performance gathering and hbayesdm

--- util/gather_performance_learningtask_ph.R
+++ util/gather_performance_learningtask_ph.R
@@ -11,7 +11,7 @@ diroutput <- paste(dirinput,"output",sep="/")
 
 #dirinput <- "O:\studies\allread\models_PH\logs"
 #diroutput <- "O:\studies\allread\models_PH\logs\output"
-#task = "fbl"
+task = "fbl"
 
 setwd(dirinput)
 files <- dir(pattern=".txt")
@@ -25,14 +25,16 @@ for (i in 1:length(files)){
     files[i],"\t", escape_double = FALSE, locale = locale(), trim_ws = TRUE, skip_empty_rows=TRUE)
   D <- D[2:dim(D)[1],] # remove first row (with LR or RL )
   D <- D[which(D$block==1|D$block==2|D$block==3|D$block==4),]  #exclude practice trials and unnecessary rows (e.g., with avg_resp). It should have now 200 x 4 = 800 rows
-  D <- cbind(as.integer(rep(substr(files[i],2,4)),dim(D)[1]),D)
-  #D<-D[D$resp!=0,] # remove 'too slow ' responses 
-  colnames(D[1]) <- "subjID"
+  D <- cbind(rep(as.integer(substr(files[i],14,15)),dim(D)[1]),D)
+  D<-D[D$resp!=0,] # remove 'too slow ' responses 
+  colnames(D)[1] <- "subjID"
   colnames(D)[grep("rt",colnames(D))] <- "RT" 
   D[grep("RT",colnames(D))] <- D[grep("RT",colnames(D))]/1000 # RTs in seconds
-  colnames(D)[grep("resp",colnames(D))] <- "choice" 
-  #D <- D[complete.cases(D),] # remove NAs
-  D <- D[,c(grep("subjID",colnames(D)),grep("RT",colnames(D))),grep("choice",colnames(D))]
+  # if frame does contain the colname resp -> change
+  # don't use grep here, because it will find all instances of 'resp' (even colnames with name respOnset) 
+  names(D)[names(D) == "resp"] <- "choice"
+  D <- D[complete.cases(D), ]
+  D <- D[,c(grep("subjID",colnames(D)),grep("choice",colnames(D)),grep("RT",colnames(D)))]
   datalist[[i]] <- D
 }
 
@@ -42,8 +44,3 @@ Gather <- data.table::rbindlist(datalist) # combine all data frames in one
 setwd(diroutput)
 write.table(Gather,file = paste("performance_all_",task,".txt",sep=""),sep="\t",row.names = FALSE,quote=FALSE)
 
-D <- read_delim(
-  files[1],"\t", escape_double = FALSE, locale = locale(), trim_ws = TRUE, skip_empty_rows=TRUE)
-D <- D[2:dim(D)[1],] # remove first row (with LR or RL )
-D <- cbind(rep(as.integer(substr(files[1],14,15)), dim(D)[1]),D) # put subj id
-rep(as.integer(substr(files[1],14,15)), dim(D)[1])
